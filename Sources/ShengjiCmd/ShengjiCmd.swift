b5e414a4a4206103b41d54ac6e57dd1ffad2c514
Wttch <wttch@wttch.com>
2026-04-10 00:18:32 +0800

    init

---
+++ Sources/ShengjiCmd/ShengjiCmd.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct ShengjiCmd {
+    static func main() {
+        print("\u{1B}[31;43mHello, world!")
+    }
+}
